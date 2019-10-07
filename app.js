b5dbfe048eb1005bd36c0621d461655e04be8253
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-10-07 20:26:58 +0100

    Add linting and fix errors

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ app.use(express.json());
 const CARD_URL_REGEX = /\/c\/([\w\d]+)/;
 
 app.post("/done", (req, res) => {
-    const cardUrl = req.body.card;
-    const match = CARD_URL_REGEX.exec(cardUrl);
-    if (match) {
-        const cardId = match[1];
-        console.log(`Updating card ${cardId}`);
-        return done(cardId)
-            .then(() => res.sendStatus(202))
-            .catch(() => res.sendStatus(500));
-    }
-    console.log(`No card URL in ${cardUrl}`);
-    return res.sendStatus(400);
+	const cardUrl = req.body.card;
+	const match = CARD_URL_REGEX.exec(cardUrl);
+	if (match) {
+		const cardId = match[1];
+		console.log(`Updating card ${cardId}`);
+		return done(cardId)
+			.then(() => res.sendStatus(202))
+			.catch(() => res.sendStatus(500));
+	}
+	console.log(`No card URL in ${cardUrl}`);
+	return res.sendStatus(400);
 });
 
 module.exports = app;
--- trello.js
+++ trello.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 
 module.exports = {
-    done: (cardId) => axios.request({
-        method: "PUT",
-        params: {
-            key: process.env.TRELLO_API_KEY,
-            token: process.env.TRELLO_API_TOKEN,
-            value: true,
-        },
-        url: `https://api.trello.com/1/cards/${cardId}/dueComplete`,
-    }),
+	done: (cardId) => axios.request({
+		method: "PUT",
+		params: {
+			key: process.env.TRELLO_API_KEY,
+			token: process.env.TRELLO_API_TOKEN,
+			value: true,
+		},
+		url: `https://api.trello.com/1/cards/${cardId}/dueComplete`,
+	}),
 };
